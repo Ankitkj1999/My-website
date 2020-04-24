1122812d20d1458c0190d5370fd1d79b05bf65f2
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-04-24 16:02:56 +0530

    change

--- sw.js
+++ sw.js
@@ -104,22 +104,21 @@ self.addEventListener('activate', evt => {
 
 // fetch events
 self.addEventListener('fetch', evt => {
-  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
-    evt.respondWith(
-      caches.match(evt.request).then(cacheRes => {
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCacheName).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            // check cached items size
-            limitCacheSize(dynamicCacheName, 150);
-            return fetchRes;
-          })
-        });
-      }).catch(() => {
-        if(evt.request.url.indexOf('.html') > -1){
-          return caches.match('/fallback.html');
-        } 
-      })
-    );
-  }
+  console.log('fetch event', evt);
+   evt.respondWith(
+     caches.match(evt.request).then(cacheRes => {
+    return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+        limitCacheSize(dynamicCacheName, 15);
+          return fetchRes;
+        })
+      });
+   }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+       return caches.match('/pages/fallback.html');
+     } 
+  })
+  );
 });
